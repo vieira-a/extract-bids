f5fed9d9838f17d5c1d8be37d78e2ee5ae75e9da
Anderson Vieira <asvieira.br@gmail.com>
2023-09-02 01:03:01 -0300

    feat: add extract bidding item mapper

--- src/infra/util/extract-bidding-item-mapper.ts
+++ src/infra/util/extract-bidding-item-mapper.ts
@@ -1,16 +1,40 @@
-import { ExtractBiddingItemsApiResponse } from '../contracts/extract-bidding-api-response';
-import { BiddingItems } from '../../domain/entities';
+import { ExtractBiddingItemsApiResponse } from '../contracts';
+import { ExtractBiddingItemsEntity } from '../../domain/entities';
 
 export function extractBiddingItemMapper(
   apiResponse: ExtractBiddingItemsApiResponse,
   biddingCode: string,
-): BiddingItems[] {
-  return apiResponse.itens.result.map((apiData) => ({
-    codigoLicitacao: biddingCode,
-    codigo: apiData.codigo,
-    descricao: apiData.descricao,
-    participacao: apiData.participacao,
-    quantidade: apiData.quantidade,
-    valorReferencia: apiData.valorReferencia,
-  }));
+): ExtractBiddingItemsEntity[] {
+  if (apiResponse.isLote && apiResponse.lotes && apiResponse.lotes.result) {
+    const lotes = apiResponse.lotes.result;
+    if (lotes.length > 0 && lotes[0].itens) {
+      const loteItens = lotes[0].itens.map((apiData) => ({
+        codigoLicitacao: biddingCode,
+        codigo: apiData.codigo,
+        descricao: apiData.descricao,
+        participacao: apiData.participacao,
+        quantidade: apiData.quantidade,
+        valorReferencia: apiData.valorReferencia,
+      }));
+
+      console.log('Itens do Lote (isLote=true):', loteItens);
+
+      return loteItens;
+    }
+  } else if (apiResponse.itens && apiResponse.itens.result) {
+    const normalItems = apiResponse.itens.result.map((apiData) => ({
+      codigoLicitacao: biddingCode,
+      codigo: apiData.codigo,
+      descricao: apiData.descricao,
+      participacao: apiData.participacao,
+      quantidade: apiData.quantidade,
+      valorReferencia: apiData.valorReferencia,
+    }));
+
+    console.log('Itens Normais (isLote=false):', normalItems);
+
+    return normalItems;
+  }
+  console.log('Nenhum item encontrado.');
+  return [];
 }
---
+++ src/infra/util/index.ts
@@ -0,0 +1,2 @@
+export * from './extract-bidding-mapper';
+export * from './extract-bidding-item-mapper';
